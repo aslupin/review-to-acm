b36873ca6f5bf907ed2671f3d150c34d500609aa
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-29 00:38:00 +0700

    fix fool bug & done the job

--- data-structure/graph.cpp
+++ data-structure/graph.cpp
@@ -4,13 +4,23 @@
 using namespace std;
 int main(){
     int e,v;
-    cin << e << v;
-    int tbl[e,e];
+    cin >> e >> v;
+    int tbl[e][e];
     for(int i=0;i<e;i++)
     {
         for (int j=0;j<e;j++)
         {
-            tbl[i,j] = -1;
+            tbl[i][j] = -1;
+        }
+    }
+    for(int i=0;i<v;i++)
+    {
+        int s,f,w;
+        cin >> s >> f >> w;
+        if(w<tbl[s][f] || tbl[s][f]==-1)
+        {
+            tbl[s][f]=w;
+            tbl[f][s]=w;
         }
     }
 }
