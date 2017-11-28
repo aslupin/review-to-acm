6f2db7b9ac3519eca6940d2baf252d0717cbbe6a
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-28 23:58:57 +0700

    forgeted

--- data-structure/graph.cpp
+++ data-structure/graph.cpp
@@ -3,5 +3,14 @@
 
 using namespace std;
 int main(){
-    
+    int e,v;
+    cin << e << v;
+    int tbl[e,e];
+    for(int i=0;i<e;i++)
+    {
+        for (int j=0;j<e;j++)
+        {
+            tbl[i,j] = -1;
+        }
+    }
 }
